aff1d3e545bff92e47e0baa72f8baacfbe350723
Armandh96 <armandh@iastate.edu>
2018-12-06 15:41:47 -0600

    minor change to collision

--- js/main.js
+++ js/main.js
@@ -33,14 +33,14 @@
   cube2.translateX(30);
   var objCube = new THREE.Object3D();
   var objCube2 = new THREE.Object3D();
-
-  var cubeNode = new node("cube", cube);
-  var cube2Node = new node("cube2", cube2);
+  objCube.add(cube);
+  objCube2.add(cube2);
+  var cubeNode = new node("cube", objCube);
+  var cube2Node = new node("cube2", objCube2);
   objectTree.setRoot(cubeNode);
   cubeNode.setChildL(cube2Node);
 
-  objCube.add(cube);
-  objCube2.add(cube2);
+
   scene.add(objCube);
   scene.add(objCube2);
 }
