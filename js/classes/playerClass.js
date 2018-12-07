efaf6a02e5a873ea936eee0ba46718be5605204d
CubedKing <BinayaShree@gmail.com>
2018-12-07 16:55:24 -0600

    Adding in comments to under stand the player collision code

--- js/classes/playerClass.js
+++ js/classes/playerClass.js
@@ -49,11 +49,10 @@ class player {
 
     //-- Movement --
     {
-      //-- onGround Movement --
+      //-- onGround Movement Calculation --
       {
         this.horizontalZ = this.moveForward - this.moveBackward;
         this.horizontalX = this.moveLeft - this.moveRight;
-        // console.log(this.horizontalZ);
         if(this.horizontalX != 0 && this.horizontalZ != 0) {
           this.currentMoveSpd = this.spd/Math.sqrt(2);
         } else {
@@ -81,114 +80,143 @@ class player {
       }
 
       //-- Apply Movement --
-      if(objectList.length > 0) {
-        for(var i = 0; i < objectList.length; i++) {
-          if(placeMeeting( this.box.min.x + this.hspdX, this.box.max.x + this.hspdX,
-                            this.box.min.z, this.box.max.z,
-                            this.box.min.y, this.box.max.y, objectList[i])) {
+      {
+        //-- Do we have objects to collide with? --
+        {
+          if(objectList.length > 0) {
 
-            if(placeMeeting( this.box.min.x + sign(this.hspdX), this.box.max.x + sign(this.hspdX),
-                              this.box.min.z, this.box.max.z,
-                              this.box.min.y, this.box.max.y, objectList[i])) {
-                              this.collisionXSign = true;
+            //-- Itterate Through the list --
+            {
+              for(var i = 0; i < objectList.length; i++) {
+
+                //-- Check for collision in the x axis --
+                {
+                  if(placeMeeting( this.box.min.x + this.hspdX, this.box.max.x + this.hspdX,
+                                    this.box.min.z, this.box.max.z,
+                                    this.box.min.y, this.box.max.y, objectList[i])) {
+
+                    if(placeMeeting( this.box.min.x + sign(this.hspdX), this.box.max.x + sign(this.hspdX),
+                                      this.box.min.z, this.box.max.z,
+                                      this.box.min.y, this.box.max.y, objectList[i])) {
+                                      this.collisionXSign = true;
+                    }
+
+                                    this.collisionX = true;
+                                    this.collisionXObject = objectList[i];
+                  }
+                }
+
+                //-- Check for collision in the z axis --
+                {
+                  if(placeMeeting( this.box.min.x, this.box.max.x,
+                                    this.box.min.z + this.hspdZ, this.box.max.z + this.hspdZ,
+                                    this.box.min.y, this.box.max.y, objectList[i])) {
+
+                    if(placeMeeting( this.box.min.x, this.box.max.x,
+                                      this.box.min.z + sign(this.hspdZ), this.box.max.z + sign(this.hspdZ),
+                                      this.box.min.y, this.box.max.y, objectList[i])) {
+                                      this.collisionZSign = true;
+                    }
+                                    this.collisionZ = true;
+                                    this.collisionZObject = objectList[i];
+                  }
+                }
+
+                //-- Check for collision in the y axis --
+                {
+                  if(placeMeeting( this.box.min.x, this.box.max.x,
+                                    this.box.min.z, this.box.max.z,
+                                    this.box.min.y + this.vspd, this.box.max.y + this.vspd, objectList[i])) {
+
+                    if(placeMeeting( this.box.min.x, this.box.max.x,
+                                      this.box.min.z + sign(this.hspdZ), this.box.max.z + sign(this.hspdZ),
+                                      this.box.min.y, this.box.max.y, objectList[i])) {
+                                      this.collisionYSign = true;
+                    }
+                                    this.collisionY = true;
+                                    this.collisionYObject = objectList[i];
+                  }
+                }
+              }
+            } else {
+              this.body.translateX(this.hspdX * 60 * delta);
+              this.body.translateZ(this.hspdZ * 60 * delta);
+              this.body.translateY(this.vspd * 60 * delta);
             }
-
-                            this.collisionX = true;
-                            this.collisionXObject = objectList[i];
           }
+        }
 
-          if(placeMeeting( this.box.min.x, this.box.max.x,
+        //-- Can we move? --
+        {
+          if(!this.collisionX && !this.collisionXSign) {
+            this.body.translateX(this.hspdX * 60 * delta);
+          }
+          if(!this.collisionY && !this.collisionYSign) {
+            this.body.translateY(this.vspd * 60 * delta);
+          }
+          if(!this.collisionZ && !this.collisionZSign) {
+            this.body.translateZ(this.hspdZ * 60 * delta);
+          }
+        }
+
+        //-- Check if there still is a collision in the x axis --
+        {
+          if(this.collisionXObject != null) {
+            if(placeMeeting(this.box.min.x + this.hspdX, this.box.max.x + this.hspdX,
+                            this.box.min.z, this.box.max.z,
+                            this.box.min.y, this.box.max.y, this.collisionXObject)) {
+                            this.collisionX = false;
+                            //this.collisionXObject = null;
+            } else if(!placeMeeting(this.box.min.x + sign(this.hspdX), this.box.max.x + sign(this.hspdX),
+                            this.box.min.z, this.box.max.z,
+                            this.box.min.y, this.box.max.y, this.collisionXObject)) {
+                            this.collisionXSign = false;
+                            this.collisionXObject = null;
+            }
+          }
+        }
+
+        //-- Check if there still is a collision in the x axis --
+        {
+          if(this.collisionZObject != null) {
+            if(placeMeeting( this.box.min.x, this.box.max.x,
                             this.box.min.z + this.hspdZ, this.box.max.z + this.hspdZ,
-                            this.box.min.y, this.box.max.y, objectList[i])) {
-
-            if(placeMeeting( this.box.min.x, this.box.max.x,
-                              this.box.min.z + sign(this.hspdZ), this.box.max.z + sign(this.hspdZ),
-                              this.box.min.y, this.box.max.y, objectList[i])) {
-                              this.collisionZSign = true;
+                            this.box.min.y, this.box.max.y, this.collisionZObject)) {
+                            this.collisionZ = false;
+                            //this.collisionZObject = null;
+            } else if(!placeMeeting(this.box.min.x, this.box.max.x,
+                            this.box.min.z + sign(this.hspdZ), this.box.max.z + sign(this.hspdZ),
+                            this.box.min.y, this.box.max.y, this.collisionZObject)) {
+                            this.collisionZSign = false;
+                            this.collisionZObject = null;
             }
-                            this.collisionZ = true;
-                            this.collisionZObject = objectList[i];
           }
+        }
 
-          if(placeMeeting( this.box.min.x, this.box.max.x,
+        //-- Check if there still is a collision in the x axis --
+        {
+          if(this.collisionYObject != null) {
+            if(placeMeeting( this.box.min.x, this.box.max.x,
                             this.box.min.z, this.box.max.z,
-                            this.box.min.y + this.vspd, this.box.max.y + this.vspd, objectList[i])) {
-
-            if(placeMeeting( this.box.min.x, this.box.max.x,
-                              this.box.min.z + sign(this.hspdZ), this.box.max.z + sign(this.hspdZ),
-                              this.box.min.y, this.box.max.y, objectList[i])) {
-                              this.collisionYSign = true;
+                            this.box.min.y + this.vspd, this.box.max.y + this.vspd, this.collisionYObject)) {
+                            this.collisionY = false;
+                            //this.collisionYObject = null;
+            } else if(!placeMeeting(this.box.min.x, this.box.max.x,
+                            this.box.min.z, this.box.max.z,
+                            this.box.min.y + sign(this.vspd), this.box.max.y + sign(this.vspd), this.collisionYObject)) {
+                            this.collisionYSign = false;
+                            this.collisionYObject = null;
             }
-                            this.collisionY = true;
-                            this.collisionYObject = objectList[i];
           }
         }
-      } else {
-        this.body.translateX(this.hspdX * 60 * delta);
-        this.body.translateZ(this.hspdZ * 60 * delta);
-        this.body.translateY(this.vspd * 60 * delta);
       }
-
-      if(!this.collisionX && !this.collisionXSign) {
-        this.body.translateX(this.hspdX * 60 * delta);
-      }
-      if(!this.collisionY && !this.collisionYSign) {
-        this.body.translateY(this.vspd * 60 * delta);
-      }
-      if(!this.collisionZ && !this.collisionZSign) {
-        this.body.translateZ(this.hspdZ * 60 * delta);
-      }
-
-      if(this.collisionXObject != null) {
-        if(placeMeeting(this.box.min.x + this.hspdX, this.box.max.x + this.hspdX,
-                        this.box.min.z, this.box.max.z,
-                        this.box.min.y, this.box.max.y, this.collisionXObject)) {
-                        this.collisionX = false;
-                        //this.collisionXObject = null;
-        } else if(!placeMeeting(this.box.min.x + sign(this.hspdX), this.box.max.x + sign(this.hspdX),
-                        this.box.min.z, this.box.max.z,
-                        this.box.min.y, this.box.max.y, this.collisionXObject)) {
-                        this.collisionXSign = false;
-                        this.collisionXObject = null;
-        }
-      }
-
-      if(this.collisionZObject != null) {
-        if(placeMeeting( this.box.min.x, this.box.max.x,
-                        this.box.min.z + this.hspdZ, this.box.max.z + this.hspdZ,
-                        this.box.min.y, this.box.max.y, this.collisionZObject)) {
-                        this.collisionZ = false;
-                        //this.collisionZObject = null;
-        } else if(!placeMeeting(this.box.min.x, this.box.max.x,
-                        this.box.min.z + sign(this.hspdZ), this.box.max.z + sign(this.hspdZ),
-                        this.box.min.y, this.box.max.y, this.collisionZObject)) {
-                        this.collisionZSign = false;
-                        this.collisionZObject = null;
-        }
-      }
-
-      if(this.collisionYObject != null) {
-        if(placeMeeting( this.box.min.x, this.box.max.x,
-                        this.box.min.z, this.box.max.z,
-                        this.box.min.y + this.vspd, this.box.max.y + this.vspd, this.collisionYObject)) {
-                        this.collisionY = false;
-                        //this.collisionYObject = null;
-        } else if(!placeMeeting(this.box.min.x, this.box.max.x,
-                        this.box.min.z, this.box.max.z,
-                        this.box.min.y + sign(this.vspd), this.box.max.y + sign(this.vspd), this.collisionYObject)) {
-                        this.collisionYSign = false;
-                        this.collisionYObject = null;
-        }
-      }
-
-
     }
 
     //-- Debug Code --
     {
       //console.log(this.body.position.z);
       //console.log(this.box.min);
-      console.log(this.collisionXSign);
+      //console.log(this.collisionXSign);
     }
 
   }
